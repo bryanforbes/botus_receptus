f04c397a21088f8b991882da980c77cebed94d58
Bryan Forbes <bryan@reigndropsfall.net>
2018-11-05 17:09:24 -0600

    Add create_or_update utility to gino module

--- src/botus_receptus/gino/__init__.py
+++ src/botus_receptus/gino/__init__.py
@@ -1,4 +1,5 @@
 from .base import db, Base, Snowflake
 from .bot import Bot
+from .util import create_or_update
 
-__all__ = ('db', 'Base', 'Snowflake', 'Bot')
+__all__ = ('db', 'Base', 'Snowflake', 'Bot', 'create_or_update')
---
+++ src/botus_receptus/gino/util.py
@@ -0,0 +1,28 @@
+from typing import Any, Union, TypeVar, Type, Sequence, Mapping, List, Dict, cast
+from gino.crud import CRUDModel
+from sqlalchemy.dialects.postgresql import insert
+
+from .base import db
+
+_CM = TypeVar('_CM', bound=CRUDModel)
+
+
+async def create_or_update(cls: Type[_CM], *, set_: Union[Sequence[str], Mapping[str, str]], **values: Any) -> _CM:
+    column_name_map = cls._column_name_map
+
+    index_elements: List[str] = [column_name_map[k] for k in cls.__table__.primary_key.columns.keys()]
+    insert_values: Dict[str, Any] = {column_name_map[k]: v for k, v in values.items()}
+
+    set_values: Mapping[str, str]
+    if hasattr(set_, 'get'):
+        set_values = cast(Mapping[str, str], set_)
+    else:
+        set_values = {v: v for v in cast(Sequence[str], set_)}
+
+    stmt = insert(cls).values(**insert_values).returning(*cls)
+    stmt = stmt.on_conflict_do_update(index_elements=index_elements,
+                                      set_={k: getattr(stmt.excluded, v) for k, v in set_values.items()}) \
+        .execution_options(return_model=False, model=cls)
+
+    row = await db.bind.first(stmt)
+    return cls(**{column_name_map.invert_get(k, ''): v for k, v in row.items()})
