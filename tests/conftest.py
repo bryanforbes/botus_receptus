6d38f711edcb8872857acf8cd0bc6e5cf55ee9f6
Bryan Forbes <bryan@reigndropsfall.net>
2018-11-08 09:08:57 -0600

    Add pytest-sugar

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,17 @@ import asynctest.mock  # type: ignore
 pytest_mock._get_mock_module._module = asynctest.mock
 
 
+def pytest_configure():
+    workaround_sugar_issue_159()
+
+
+def workaround_sugar_issue_159():
+    "https://github.com/Frozenball/pytest-sugar/issues/159"
+    import pytest_sugar
+
+    pytest_sugar.SugarTerminalReporter.pytest_runtest_logfinish = lambda self: None
+
+
 @pytest.fixture
 def mock_aiohttp(mocker: Any) -> None:
     mocker.patch('aiohttp.ClientSession', autospec=True)
