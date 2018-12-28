cc89ad343ac464373c034260940be357e047c632
Bryan Forbes <bryan@reigndropsfall.net>
2018-12-27 23:33:46 -0600

    Add typings for more_itertools and remove unique_seen

--- src/botus_receptus/util.py
+++ src/botus_receptus/util.py
@@ -1,28 +1,7 @@
 from __future__ import annotations
 
-from typing import TypeVar, Iterable, Optional, Callable, Any, Iterator, Set, Container
-from itertools import filterfalse
-
+from typing import Container
 import discord
-
-T = TypeVar('T')
-
-
-def unique_seen(
-    iterable: Iterable[T], get_key: Optional[Callable[[T], Any]] = None
-) -> Iterator[T]:
-    seen: Set[T] = set()
-    seen_add: Callable[[T], None] = seen.add
-    if get_key is None:
-        for element in filterfalse(seen.__contains__, iterable):
-            seen_add(element)
-            yield element
-    else:
-        for element in iterable:
-            key = get_key(element)
-            if key not in seen:
-                seen_add(key)
-                yield element
 
 
 def has_any_role(member: discord.Member, roles: Container[str]) -> bool:
