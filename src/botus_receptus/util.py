01d4bea4569a754a8b054658d815bfed73944a39
Bryan Forbes <bryan@reigndropsfall.net>
2019-01-08 17:56:23 -0600

    Add utils.parse_duration()

--- src/botus_receptus/util.py
+++ src/botus_receptus/util.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from typing import Container
+from typing import Container, Dict
 import discord
+import pendulum  # type: ignore
 
 
 def has_any_role(member: discord.Member, roles: Container[str]) -> bool:
@@ -10,3 +11,35 @@ def has_any_role(member: discord.Member, roles: Container[str]) -> bool:
 
 def has_any_role_id(member: discord.Member, ids: Container[int]) -> bool:
     return discord.utils.find(lambda role: role.id in ids, member.roles) is not None
+
+
+UNITS = {
+    'h': 'hours',
+    'm': 'minutes',
+    's': 'seconds',
+    'd': 'days',
+    'w': 'weeks',
+    'y': 'years',
+}
+
+
+# Adapted from https://github.com/python-discord/site/blob/master/pysite/utils/time.py
+def parse_duration(duration: str) -> pendulum.Duration:  # type: ignore
+    if not duration:
+        raise ValueError('No duration provided.')
+
+    args: Dict[str, int] = {}
+    digits = ''
+
+    for char in duration:
+        if char.isdigit():
+            digits += char
+            continue
+
+        if char not in UNITS or not digits:
+            raise ValueError('Invalid duration')
+
+        args[UNITS[char]] = int(digits)
+        digits = ''
+
+    return pendulum.duration(**args)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,8 @@
+import pytest  # type: ignore
 import attr
+import pendulum  # type: ignore
 
-from botus_receptus.util import has_any_role, has_any_role_id
+from botus_receptus.util import has_any_role, has_any_role_id, parse_duration
 
 
 @attr.s(slots=True, auto_attribs=True)
@@ -36,3 +38,22 @@ def test_has_any_role_id() -> None:
     assert not has_any_role_id(
         MockMember(roles=[MockRole(id=1), MockRole(id=2), MockRole(id=3)]), {28, 4, 18}
     )
+
+
+@pytest.mark.parametrize(
+    'duration,expected',
+    [
+        ('42s', pendulum.duration(seconds=42)),
+        ('12m', pendulum.duration(minutes=12)),
+        ('20h', pendulum.duration(hours=20)),
+        ('7d', pendulum.duration(days=7)),
+        ('2w', pendulum.duration(weeks=2)),
+        ('1y', pendulum.duration(years=1)),
+        ('12m30s', pendulum.duration(seconds=30, minutes=12)),
+        ('20h5m', pendulum.duration(hours=20, minutes=5)),
+        ('7d10h12s', pendulum.duration(seconds=12, days=7, hours=10)),
+        ('2m1y', pendulum.duration(years=1, seconds=120)),
+    ],
+)
+def test_parse_duration(duration, expected):
+    assert parse_duration(duration) == expected
