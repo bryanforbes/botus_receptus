d27ff8fb28c1ddab05760a00742bc90d5ade1b83
Bryan Forbes <bryan@reigndropsfall.net>
2020-04-08 12:16:11 -0500

    Fixes for decorators

--- src/botus_receptus/checks.py
+++ src/botus_receptus/checks.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import discord
 from discord.ext import commands
-from discord.ext.commands import core
 
 from .exceptions import NotGuildOwner, OnlyDirectMessage
 
+if TYPE_CHECKING:
+    from discord.ext.commands import core
+
 
 def dm_only() -> 'core._CheckDecorator':
-    def predicate(ctx: commands.Context) -> bool:
+    def predicate(ctx: 'core._CT') -> bool:
         if not isinstance(ctx.channel, discord.DMChannel):
             raise OnlyDirectMessage('This command can only be used in private messags.')
         return True
@@ -17,7 +21,7 @@ def dm_only() -> 'core._CheckDecorator':
 
 
 def is_guild_owner() -> 'core._CheckDecorator':
-    def predicate(ctx: commands.Context) -> bool:
+    def predicate(ctx: 'core._CT') -> bool:
         if ctx.guild is None:
             raise commands.NoPrivateMessage(
                 'This command cannot be used in private messages.'
