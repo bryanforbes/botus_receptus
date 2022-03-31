b3c7896f99229c4e2d70097d14d31967674d143d
Bryan Forbes <bryan@reigndropsfall.net>
2022-03-31 11:13:46 -0500

    Remove error helper and do it on Cog

--- src/botus_receptus/cog.py
+++ src/botus_receptus/cog.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from collections.abc import Awaitable, Iterable
-from typing import TYPE_CHECKING, Generic, TypeVar
+from collections.abc import Awaitable, Coroutine, Iterable
+from typing import TYPE_CHECKING, Any, Generic, TypeAlias, TypeVar
 
+import discord
+from discord import app_commands
 from discord.abc import Snowflake
 from discord.ext import commands
+from discord.ext.commands import cog  # type: ignore
 from discord.utils import maybe_coroutine
 
 from .bot import AutoShardedBot, Bot
@@ -13,7 +16,9 @@ if TYPE_CHECKING:
     from typing_extensions import Self
 
 
+_T = TypeVar('_T')
 _BotT = TypeVar('_BotT', bound=Bot | AutoShardedBot)
+_Coro: TypeAlias = Coroutine[Any, Any, _T]
 
 
 class Cog(commands.Cog, Generic[_BotT]):
@@ -23,6 +28,27 @@ class Cog(commands.Cog, Generic[_BotT]):
         super().__init__()
 
         self.bot = bot
+
+        on_error = commands.Cog._get_overridden_method(self.cog_app_command_error)
+
+        if on_error is not None:
+            for command in self.__cog_app_commands__:
+                if command.parent is None:
+                    self._set_command_error_handler(command)
+
+    def _set_command_error_handler(
+        self,
+        command: app_commands.Command[Self, ..., Any] | app_commands.Group,
+        /,
+    ) -> None:
+        if isinstance(command, app_commands.Group):
+            command.on_error = cog.cog_app_command_error  # type: ignore
+            return
+
+        def on_error(interaction: discord.Interaction, error: Exception) -> _Coro[Any]:
+            return self.cog_app_command_error(interaction, command, error)
+
+        command.on_error = on_error
 
     async def _inject(  # type: ignore
         self,
@@ -84,4 +110,14 @@ class Cog(commands.Cog, Generic[_BotT]):
         guild_ids: Iterable[int] | None,
         /,
     ) -> None | Awaitable[None]:
+        ...
+
+    @cog._cog_special_method
+    async def cog_app_command_error(
+        self,
+        interaction: discord.Interaction,
+        command: app_commands.Command[Self, ..., Any],
+        error: Exception,
+        /,
+    ) -> None:
         ...
